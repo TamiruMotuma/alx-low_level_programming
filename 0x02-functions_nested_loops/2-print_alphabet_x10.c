e5d0274439ce941e8b21c5fbb24fc6ecf0b8013c
TamiruMotuma <rajiimotuma@gmail.com>
2022-12-14 01:28:43 -0800

    10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print the alphabet in lowercase
+ */
+void print_alphabet_x10(void);
+{
+	char la;
+
+	for (la = 'a'; la <= 'z'; la++)
+		_putchar(la);
+
+	_putchar('\n');
+}
